c6488ea281c6f562ca2671e3ea8a7b62b926e25b
Cheng Lou <chenglou92@gmail.com>
2014-09-21 13:50:47 -0400

    Solve tic-tac-toe even better
    Arbitrary board size; smarter algo

--- tic-tac-toe-solved/isSolved.cljs
+++ tic-tac-toe-solved/isSolved.cljs
@@ -1,47 +1,61 @@
-(def sols [[[0 0] [0 1] [0 2]]
-           [[1 0] [1 1] [1 2]]
-           [[2 0] [2 1] [2 2]]
-           [[0 0] [1 0] [2 0]]
-           [[0 1] [1 1] [2 1]]
-           [[0 2] [1 2] [2 2]]
-           [[0 0] [1 1] [2 2]]
-           [[0 2] [1 1] [2 0]]])
+; arbitrary size tic-tac-toe board; check the state of the game
 
-(def testa [[0 1 1]
+(defn toVec [mat] (into [] (map (partial into []) mat)))
+
+;transpose + reverse each row
+(defn rotate [mat] (toVec (map rseq (apply (partial map vector) mat))))
+
+(defn get-diag [mat] (for [i (range (count mat))] (get-in mat [i i])))
+
+(defn check-row [row n] (apply = (conj row n)))
+
+(defn check-diag [mat n] (check-row (get-diag mat) n))
+
+(defn has0 [mat] (some (fn [r] (some #(= % 0) r)) mat))
+
+(defn check-row-and-diag [board n]
+  (or (some #(check-row % n) board)
+      (check-diag board n)))
+
+(defn is-solved-for-num [board n]
+  (or (check-row-and-diag board n)
+      (check-row-and-diag (rotate board) n)))
+
+(defn is-solved [board]
+  (cond
+    (is-solved-for-num board 1) 1 ;1 wins
+    (is-solved-for-num board 2) 2 ;2 wins
+    (has0 board) -1 ;unfinished
+    :else 0)) ;draw
+
+;=============
+
+(def test1 [[0 1 1]
             [0 1 0]
             [1 2 2]])
 
-(def testa2 [[2 1 1]
+(def test2 [[2 1 1]
              [2 1 0]
              [2 0 2]])
 
-(def testa3 [[0 1 1]
+(def test3 [[0 1 1]
              [0 1 0]
              [2 0 2]])
 
-(def testa4 [[2 1 1]
+(def test4 [[2 1 1]
              [1 1 2]
              [2 2 1]])
 
-(defn _isSolved [b n]
-  (some #(let [c0 (get-in b [((% 0) 0) ((% 0) 1)])
-                c1 (get-in b [((% 1) 0) ((% 1) 1)])
-                c2 (get-in b [((% 2) 0) ((% 2) 1)])]
-           (= c0 c1 c2 n))
-        sols))
+(def test5 [[2 1 1 0]
+            [1 1 2 0]
+            [2 1 1 0]
+            [1 1 1 1]])
 
-(defn has0 [b] (some (fn [r] (some #(= % 0) r)) b))
+(def test6 [[2]])
 
-(defn isSolved [b]
-  (let [win1 (_isSolved b 1)
-        win2 (_isSolved b 2)]
-    (cond
-      win1 1
-      win2 2
-      (has 0) -1
-      :else 0)))
-
-(isSolved testa) ;1
-(isSolved testa2) ;2
-(isSolved testa3) ;-1
-(isSolved testa4) ;0
+(assert (= (is-solved test1) 1))
+(assert (= (is-solved test2) 2))
+(assert (= (is-solved test3) -1))
+(assert (= (is-solved test4) 0))
+(assert (= (is-solved test5) 1))
+(assert (= (is-solved test6) 2))
