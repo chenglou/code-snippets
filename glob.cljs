3bb175f506eb4f210ad6fa5cfcd9afb57f505b0e
Cheng Lou <chenglou92@gmail.com>
2014-10-14 23:13:19 -0400

    Glob pattern in cljs

---
+++ glob.cljs
@@ -0,0 +1,23 @@
+(use '[clojure.core.match :only (match)])
+
+(defn r-match [[r & rs :as R] [s & ss :as S]]
+  (match [r s]
+    [nil _] true
+    [\* _] (or (r-match rs ss) (r-match rs S) (r-match R ss))
+    [_ nil] false
+    [\. _] (r-match rs ss)
+    :else (and (= r s) (r-match rs ss))))
+
+;-----------
+(assert (r-match "" "a"))
+(assert (not (r-match "b" "a")))
+(assert (not (r-match "a" "")))
+(assert (r-match "abc" "abcd"))
+(assert (r-match "*" ""))
+(assert (r-match "*" "a"))
+(assert (r-match "*b" "b"))
+(assert (r-match "*b*" "b"))
+(assert (r-match "a*b" "acab"))
+(assert (not (r-match "." "")))
+(assert (r-match "a.b" "acb"))
+(assert (not (r-match "a.b" "accb")))
---
+++ project.clj
@@ -0,0 +1,2 @@
+(defproject code-snippets "0.0.0"
+  :dependencies [[org.clojure/core.match "0.2.1"]])
