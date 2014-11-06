36a908f87d29f22b2d87a111a4e5c78feaa30e3d
Cheng Lou <chenglou92@gmail.com>
2014-11-06 16:09:28 -0500

    Improve fibs cljs

--- Algorithms/Fibonacci/fib.cljs
+++ Algorithms/Fibonacci/fib.cljs
@@ -1,11 +1,16 @@
-(defn fib-impl [n mem]
-  (if (@mem n)
-    (@mem n)
-    (do
-      (swap! mem assoc n (+ (fib-impl (- n 1) mem) (fib-impl (- n 2) mem)))
-      (@mem n))))
+; first iteration
+; (defn fib-impl [n mem]
+;   (if (@mem n)
+;     (@mem n)
+;     (do
+;       (swap! mem assoc n (+ (fib-impl (- n 1) mem) (fib-impl (- n 2) mem)))
+;       (@mem n))))
 
-(defn fib [n] (let [mem (atom {0 0 1 1})] (fib-impl n mem)))
+; (defn fib [n] (let [mem (atom {0 0 1 1})] (fib-impl n mem)))
+
+; second iteration
+(defn gen-fib [a b] (cons a (lazy-seq (gen-fib b (+ a b)))))
+(defn fib [n] (nth (gen-fib 0 1) n))
 
 ;-------------
 (assert (= (fib 0) 0))
